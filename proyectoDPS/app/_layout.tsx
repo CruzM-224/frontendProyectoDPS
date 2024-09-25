de8c8b559323e3dc69b4807e34e9089f899e92f1
Franckalv <franciscozaalvarez@gmail.com>
2024-09-24 18:59:31 -0600

    !Syntax changes

--- proyectoDPS/app/_layout.tsx
+++ proyectoDPS/app/_layout.tsx
@@ -25,10 +25,9 @@ export default function Layout() {
   }
 
   return (
-    <>
+    
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
       </Stack>
-    </>
-  );
+      );
 }
