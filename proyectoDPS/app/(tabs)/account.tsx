7d857af9f5575fcecd7daf3e0d4d9a91b7e6abed
CruzM-224 <cruzmoreno022002@gmail.com>
2024-10-01 21:33:22 -0600

    menu v0.42, account tab

--- proyectoDPS/app/(tabs)/account.tsx
+++ proyectoDPS/app/(tabs)/account.tsx
@@ -1,9 +1,56 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+
+interface AccountSettingProps {
+  icon: string,
+  text: string,
+}
+
+const AccountSetting = ({ icon, text }: AccountSettingProps) => {
+  return (
+    <View style={styles.accountSetting}>
+      <View style={styles.leftContent}>
+        <View style={styles.icon}>
+        <FontAwesome6 name={icon} size={24} color="black" />
+        </View>
+        <Text style={styles.bodyText}>{text}</Text>
+      </View>
+      <View style={styles.rightContent}>
+        <FontAwesome6 name="angle-right" size={24} color="black" />
+      </View>
+    </View>
+  );
+}
 
 export default function Tab() {
   return (
     <View style={styles.container}>
-      <Text>Tab [Home|Settings]</Text>
+      <View style={styles.header}>
+        <View style={styles.profilePic}>
+          <FontAwesome name="user-circle-o" size={50} color="black" />
+        </View>
+        <View style={styles.containerHeaderText}>
+          <Text style={styles.Name}>Jose Perez</Text>
+          <Text style={styles.Email}>jose_perez@gmail.com</Text>
+        </View>
+      </View>
+      <View style={styles.containerBody}>
+        <AccountSetting icon="truck-ramp-box" text="My orders" />
+        <AccountSetting icon="id-card" text="My details" />
+        <AccountSetting icon="location-dot" text="Delivery address" />
+        <AccountSetting icon="credit-card" text="Payment methods" />
+        <AccountSetting icon="ticket" text="Promo Code" />
+        <AccountSetting icon="bell" text="Notifications" />
+        <AccountSetting icon="circle-question" text="Help" />
+        <AccountSetting icon="circle-info" text="About" />
+      </View>
+      <TouchableOpacity style={styles.logOutButton}>
+        <View style={styles.iconSignOut}>
+          <FontAwesome name="sign-out" size={24} color="white" />
+        </View>
+        <Text style={styles.logOutButtonText}>Log out</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -13,5 +60,78 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  header: {
+    flexDirection: 'row',
+    padding: 20,
+    width: '90%',
+    justifyContent: 'center',
+    borderBottomWidth: 1,
+  },
+  profilePic: {
+    width: 50,
+    height: 50,
+    borderRadius: 100,
+  },
+  containerHeaderText: {
+    marginLeft: 20,
+  },
+  Name: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  Email: {
+    fontSize: 12,
+  },
+  containerBody: {
+    width: '90%',
+  },
+  accountSetting: {
+    flexDirection: 'row',
+    padding: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: 'gray',
+    width: '100%',
+    height: 60,
+  },
+  leftContent: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  icon: {
+    width: 50,
+    height: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  rightContent: {
+    marginLeft: 'auto',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  bodyText: {
+    marginLeft: 10,
+  },
+  logOutButton: {
+    backgroundColor: '#DB4444',
+    borderRadius: 100,
+    width: '70%',
+    marginTop: 20,
+    padding: 10,
+    alignItems: 'center',
+    marginBottom: 20,
+    position: 'relative',
+    height: 50,
+  },
+  iconSignOut: {
+    position: 'absolute',
+    left: 20,
+    bottom: 14,
+  },
+  logOutButtonText: {
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: 'bold',
   },
 });
